1336c10e48849e0f50c3d412be107e12276b2b5a
Irfan Razi <15.irfan.razi@gmail.com>
2022-03-25 23:15:34 +0530

    stage 2 completed

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,12 +1,15 @@
 import os.path
 
+from tensorboard import summary
+
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG_16_model
+from src.utils.models import get_VGG_16_model, prepare_model
 import argparse
 import pandas as pd
 import shutil
 from tqdm import tqdm
 import logging
+import io
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
@@ -41,6 +44,24 @@ def prepare_base_model(config_path, params_path):
 
     model = get_VGG_16_model(input_shape= params["IMAGE_SIZE"], model_path=base_model_path)
 
+    full_model = prepare_model(
+        model,
+        CLASSES=params["CLASSES"],
+        freeze_all=True,
+        freeze_till=None,
+        learning_rate=params["LEARNING_RATE"]
+    )
+
+    update_base_model_path = os.path.join(base_model_dir_path, artifacts["UPDATED_BASE_MODEL_NAME"])
+
+    def _log_model_summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    logging.info(f"Full model summary: \n {_log_model_summary(full_model)}")
+    full_model.save(update_base_model_path)
 
 if __name__=='__main__':
     args = argparse.ArgumentParser()
---
+++ src/utils/models.py
@@ -0,0 +1,41 @@
+from matplotlib.cbook import flatten
+import tensorflow as tf
+import os
+import logging
+
+def get_VGG_16_model(input_shape, model_path):
+    model = tf.keras.applications.VGG16(
+        input_shape=input_shape,
+        include_top=False,
+        weights='imagenet'
+        )
+
+    model.save(model_path)
+    logging.info(f"Model VGG16 has been saved to {model_path}")
+    return model
+
+def prepare_model(model, CLASSES, freeze_all, freeze_till, learning_rate):
+    if freeze_all:
+        for layer in model.layers:
+            layer.trainable = False
+    elif (freeze_till is not None) or (freeze_till>0):
+        for layer in model.layers[:freeze_till]:
+            layer.trainable = False
+    
+    flatten_in = tf.keras.layers.Flatten()(model.output)
+    prediction =tf.keras.layers.Dense(
+        units=CLASSES,
+        activation='softmax'
+    )(flatten_in)
+
+    full_model = tf.keras.models.Model(inputs=model.input, outputs=prediction)
+
+    full_model.compile(
+        optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=['accuracy']
+    )
+
+    logging.info("Custom model is ready and ready to be trained.")
+
+    return full_model
